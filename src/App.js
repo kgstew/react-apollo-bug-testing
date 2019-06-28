beb71fdb1c45b2a3f77b0d56b4e20c5bd3e7bb6b
Kyle Stewart <kgstew@gmail.com>
2019-06-27 22:21:24 -0700

    Use query component instead of compsoe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Query, compose, graphql } from 'react-apollo';
+import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
 const ALL_PEOPLE = gql`
@@ -13,32 +13,30 @@ const ALL_PEOPLE = gql`
 `;
 
 const App = props => {
-    const { loading, data } = props;
-    const { people } = data;
-
-    if (loading || !data) {
-        return <p>Loading</p>;
-    }
-
-    console.log('data', data);
-
     return (
-        <main>
-            <h1>Apollo Client Error Template</h1>
-            <p>
-                This is a template that you can use to demonstrate an error in Apollo Client. Edit the source code and
-                watch your browser window reload with the changes.
-            </p>
-            <p>
-                The code which renders this component lives in <code>./src/App.js</code>.
-            </p>
-            <p>
-                The GraphQL schema is in <code>./src/graphql/schema</code>. Currently the schema just serves a list of
-                people with names and ids.
-            </p>
-            <ul>{!people ? 'Foo' : people.map(person => <li key={person.id}>{person.name}</li>)}</ul>
-        </main>
+        <Query query={ALL_PEOPLE}>
+            {({ loading, data }) => {
+                if (loading || !data) {
+                    return <p>Loading</p>;
+                }
+
+                console.log('data', data);
+
+                const { people } = data;
+
+                return (
+                    <main>
+                        <h1>Apollo Client Error Template</h1>
+                        <p>
+                            Data returns and empty object when fetching a field using the @client directive when that
+                            field does not yet exist in the cache and does not have a resolver.
+                        </p>
+                        <ul>{!people ? 'No Data' : people.map(person => <li key={person.id}>{person.name}</li>)}</ul>
+                    </main>
+                );
+            }}
+        </Query>
     );
 };
 
-export default compose(graphql(ALL_PEOPLE))(App);
+export default App;
